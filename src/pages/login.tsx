21547a60d0d94e3c509bc33fe77f2347c4535381
phyuna0525 <128463899+phyuna0525@users.noreply.github.com>
2024-01-20 00:04:33 +0900

    Fix : 에러 수정

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,63 +1,61 @@
 import styled from "styled-components";
-import Logoimg from "../assets/imgs/logo1.svg"
+import Logoimg from "../assets/imgs/logo1.svg";
 // import { theme } from "../styles/theme";
 
 export function Login() {
-    return (
-        <Wrapper>
-            <CommentWrapper>
-                <Comment>
-                    <Name></Name>
-                    <Content></Content>
-                </Comment>
-            </CommentWrapper>
-            <LoginWrapper>
-                <Logo src={Logoimg}></Logo>
-            </LoginWrapper>
-            {/* <Ani theme={theme}></Ani> */}
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <CommentWrapper>
+        <Comment>
+          <Name></Name>
+          <Content></Content>
+        </Comment>
+      </CommentWrapper>
+      <LoginWrapper>
+        <Logo src={Logoimg}></Logo>
+      </LoginWrapper>
+      {/* <Ani theme={theme}></Ani> */}
+    </Wrapper>
+  );
 }
 
-
 const Logo = styled.img`
-    width: 50px;
+  width: 50px;
 `;
 
 const LoginWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 400px;
-    height: 480px;
-    border: solid ${({ theme }) => theme.color.gray__1} 1px;
-    border-radius: 10px;
-    margin: auto;
-    display: flex;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  height: 480px;
+  border: solid ${({ theme }) => theme.color.gray__1} 1px;
+  border-radius: 10px;
+  margin: auto;
+  display: flex;
+  align-items: center;
 `;
 const Name = styled.div``;
 const Content = styled.div``;
 
 const Comment = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 const CommentWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 43%;
-    background-color: #FFDEDE;
-    padding: 30px;
-    box-sizing: border-box;
-    height: 100vh;
+  display: flex;
+  flex-direction: column;
+  width: 43%;
+  background-color: #ffdede;
+  padding: 30px;
+  box-sizing: border-box;
+  height: 100vh;
 `;
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 `;
-
 
 // const Ani = styled.div`
 //     width: 30px;
