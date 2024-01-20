3541e49c7be1ab428e953230e9e22c59a1f7dcc6
phyuna0525 <128463899+phyuna0525@users.noreply.github.com>
2024-01-20 12:47:50 +0900

    test

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,16 @@
-// 파일 내용...
+import styled from "styled-components";
+import { Icon } from "../assets/imgs/Icons/celender";
+import { theme } from "../styles/theme";
 
-export { };
+interface IconProps {
+  stroke: string;
+}
+
+export function Header() {
+  return <Icon src={Icon} stroke={theme.color.four} color={theme.color.five} />;
+}
+
+const Icon = styled.img<IconProps>`
+  color: ${({ theme }) => theme.color.Red_1};
+  stroke: ${({ stroke }) => theme.color.one};
+`;
