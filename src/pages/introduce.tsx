2d740e1480d468bd8bcaa0bd217a8dc03ab137a2
phyuna0525 <128463899+phyuna0525@users.noreply.github.com>
2024-01-28 22:26:09 +0900

    fix : center

--- src/pages/introduce.tsx
+++ src/pages/introduce.tsx
@@ -3,30 +3,29 @@ import React from "react";
 import { Header } from "../components/header";
 
 export function Introduce() {
-    return (
-        <>
-            <Header />
-            <Wrapper>
-                <Box1>
-                    {/* introduceComp를 이용하거나 여기서 바로 퍼블리싱 */}
-                </Box1>
-            </Wrapper>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <Box1>{/* introduceComp를 이용하거나 여기서 바로 퍼블리싱 */}</Box1>
+      </Wrapper>
+    </>
+  );
 }
 
 const Box1 = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 327px;
-    height: 735px;
-    background-color: black;
+  display: flex;
+  flex-direction: column;
+  width: 327px;
+  height: 735px;
+  background-color: black;
 `;
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 75px;
-    justify-content: center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 75px;
+  justify-content: center;
 `;
