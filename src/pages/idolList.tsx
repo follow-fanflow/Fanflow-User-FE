c88fab29560a159f6f78bdc16d61b6393f2b566f
이의진 <aghpt04@gmail.com>
2024-02-02 22:27:40 +0900

    Add : introduce file 추가

--- src/pages/idolList.tsx
+++ src/pages/idolList.tsx
@@ -4,29 +4,12 @@ import { Header } from "../components/header";
 import { Circles } from "../components/introduceIdol/circle2";
 
 export function IdolList() {
-    const [groupCount, setGroupCount] = useState(0);
-
-    const fetchGroupCount = async () => {
-        try {
-            const response = await fetch("https://api.example/groupCount");
-            const data = await response.json();
-
-            setGroupCount(data.groupCount);
-        } catch (error) {
-            console.error("에러에러: ", error);
-        }
-    }
-
-    useEffect(() => {
-        fetchGroupCount();
-    }, []);
-
     return (
         <>
             <Header />
             <Container>
                 <Wrapper>
-                    <GroupCount>팬플로우에서 {groupCount}팀의 아티스트를 만나볼 수 있어요</GroupCount>
+                    <GroupCount>팬플로우에서 16팀의 아티스트를 만나볼 수 있어요</GroupCount>
                     <CircleWrapper>
                         <Circles />
                     </CircleWrapper>
